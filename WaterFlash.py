fa4e20df5328ac2fa855af0847e9b0cb9f09ee8d
Vinícius Sz <84037924+VncsSz@users.noreply.github.com>
2024-02-04 18:54:39 -0300

    Update WaterFlash.py
    Adição de comentários

--- WaterFlash.py
+++ WaterFlash.py
@@ -1,7 +1,8 @@
 import random
 import copy
 random.seed(11)
-
+# Aqui começa o cadastro dos pedidos dos clientes.
+#Criação da classe cliente e seus atributos.
 class Cliente:
     def __init__(self, nome, quantidade, x, y):
         self.nome = nome
@@ -11,7 +12,7 @@ class Cliente:
     def __repr__(self):
         return f'Cliente({self.nome}, {self.quantidade}, {repr(self.coordenadas)})'
 
-
+#Criação das coordenadas.
 class Ponto:
     def __init__(self, x, y):
         self.x = x
@@ -23,7 +24,7 @@ class Ponto:
 
 def repr_ponto(ponto):
     return (ponto.x, ponto.y)
-
+#Array de clientes.
 listaClientes = [
     Cliente(1, 4, 14, 2),
     Cliente(2, 4, 5, 6),
@@ -32,7 +33,7 @@ listaClientes = [
     Cliente(5, 3, 1, 2),
     Cliente(6, 2, 7, 7),
 ]
-
+#Gera uma população inicial para o algoritmo genético, onde cada indivíduo é representado por um caminho aleatório entre os clientes.
 def criaGeracao(listaClientes):
     listaGeracao = []
     for i in range(10):
@@ -40,12 +41,12 @@ def criaGeracao(listaClientes):
 
     return listaGeracao
 
-
+#Calcula a quantidade total de mercadorias a serem entregues em todos os clientes
 def totalPedidos(listaClientes):
     soma_elementos = sum(cliente.quantidade for cliente in listaClientes)
     return soma_elementos
 
-
+#Gera um caminho aleatório entre os clientes.
 def gerarArrayAleatorio(listaClientes):
     voltasSede = len(listaClientes)
     quantiaVoltasSedeAleatorio = random.randint(voltasSede, voltasSede + 5)
@@ -58,16 +59,16 @@ def gerarArrayAleatorio(listaClientes):
     clientes_embaralhados = random.sample(clientes_copy, len(clientes_copy))
     return [Cliente(0, 0, 0, 0)] + clientes_embaralhados
 
-
+# Calcula a distância euclidiana entre dois pontos.
 def getDistancia(posCliente1, posCliente2):
     distancia = ((posCliente1.x - posCliente2.x) * 2 + (posCliente1.y - posCliente2.y) * 2)
     return distancia
 
-
+#Calcula o tempo de tolerância para a entrega com base na distância entre a sede e o cliente.
 def getTempoTolerancia(posSede, posCliente):
     return getDistancia(posSede, posCliente) * 1.5
 
-
+#Calcula a satisfação do cliente com base no tempo de entrega e na quantidade de águas.
 def getSatisfacao(posSede, posCliente, tempoEntrega, quantidade):
     tempoTolerancia = getTempoTolerancia(posSede, posCliente)
 
@@ -94,31 +95,30 @@ def getSatisfacao(posSede, posCliente, tempoEntrega, quantidade):
         return 1
     else:
         return 0
-
+#erifica se um cliente é a sede (coordenadas 0,0)
 def isSede(cliente):
     if cliente.coordenadas.x == 0 and (cliente.coordenadas.y == 0):
         return True
     return False
+#Calcula a satisfação total de uma solução (caminho) considerando restrições de tempo e quantidade de mercadorias.
 def calcSatisfacao(clientes):
    
     tempo_atual = 0
     carga_atual = 4
     satisfacao_total = 0
-
     
-    #print(clientes[0], "\n")
+    
     cliente_teste = clientes[0]
     for clin in cliente_teste:
-        #print("carga atual", carga_atual)
-       # print("cliente atual", clin)
+        
         pos_sede = Cliente(0, 0, 0, 0).coordenadas
         pos_cliente = clin.coordenadas
 
         distancia = getDistancia(pos_sede, pos_cliente)
-        #print("distancia", distancia)
+        
         tempo_entrega = distancia * 0.5
         tempo_tolerancia = getTempoTolerancia(pos_sede, pos_cliente)
-        #print("carga_atual < clin.quantidade", carga_atual , clin.quantidade)
+    
         if(isSede(clin)):
             carga_atual = 4
             
@@ -135,11 +135,8 @@ def calcSatisfacao(clientes):
             tempo_atual += tempo_entrega
             carga_atual -= clin.quantidade
 
-        #if cliente_teste.nome == 0:
-           # carga_atual = 4
-    #print("satisfacao_total", satisfacao_total)
     return satisfacao_total
-
+#Realiza uma mutação em um indivíduo trocando aleatoriamente parte do caminho.
 def mutacao(individuo):
     ponto_corte1 = random.randint(0, len(individuo) - 2)
     ponto_corte2 = random.randint(ponto_corte1 + 1, len(individuo) - 1)
@@ -150,7 +147,7 @@ def mutacao(individuo):
 
 
 
-
+#Realiza o crossover entre dois indivíduos para gerar dois filhos.
 def crossover(pai1, pai2):
     ponto_corte1 = random.randint(1, len(pai1))
     filho1 = pai1[:ponto_corte1] + pai2[ponto_corte1:] 
@@ -158,28 +155,28 @@ def crossover(pai1, pai2):
     return filho1, filho2
 
 
-
+#Seleciona os melhores indivíduos da população para serem mantidos na próxima geração.
 def elitismo(populacao_ordenada, elitismo_size):
     melhores_individuos = [individuo for individuo, _ in populacao_ordenada[:elitismo_size]]
     return melhores_individuos
 
 
-# Exemplo de uso
-# Exemplo de uso
+#Aqui inicia uma população inicial
 populacao = criaGeracao(listaClientes)
 
 melhor_individuo = None
 melhor_satisfacao = -10000
 
-for geracao in range(1000):  # Ajuste o número de gerações conforme necessário
+for geracao in range(100):  # Ajuste o número de gerações conforme necessário
+
     print("Geracao", geracao)
-    
+    #Dentro de cada geração, calcula a satisfação para cada indivíduo na população.
     satisfacao_por_individuo = []
     for i in populacao:
         satisfacao_por_individuo.append([ i, calcSatisfacao(i) ] )
-    
+    #Ordena a população com base na satisfação, armazenando o melhor indivíduo e sua satisfação.
     populacao_ordenada = sorted(satisfacao_por_individuo, key=lambda x: x[1], reverse=True)
-    
+    #Atualiza o melhor indivíduo se um novo melhor for encontrado.
     fitness_melhor = populacao_ordenada[0][-1]
     print(fitness_melhor)
    
@@ -187,25 +184,25 @@ for geracao in range(1000):  # Ajuste o número de gerações conforme necessár
     if fitness_melhor > melhor_satisfacao:
         melhor_satisfacao = fitness_melhor
         melhor_individuo = copy.deepcopy(populacao_ordenada[0])
-    
+    #Seleciona os melhores indivíduos (elitismo).
     #melhores_individuos = elitismo(populacao_ordenada, elitismo_size)
     nova_geracao = []
     pai1 = populacao_ordenada[0][0]
     pai2 = populacao_ordenada[1][0]
     nova_geracao.append(pai1)
     nova_geracao.append(pai2)
-    
+    #Realiza crossover e mutação para gerar a próxima geração.
     filho1, filho2 = crossover(pai1, pai2)
     nova_geracao.append(filho1)
     nova_geracao.append(filho2)
     
     for _ in range(0,6):
         nova_geracao.append( [ gerarArrayAleatorio(listaClientes), 0] )
- 
+    #Atualiza a população com a nova geração.
     populacao = nova_geracao
 
     
-
+#Imprime o melhor caminho e a satisfação total ao final das iterações.
 print("\nMelhor Caminho:")
 print(melhor_individuo)
 print(f"Satisfação Total: {melhor_satisfacao}")
